6dec88e721d885dbcad6cda8f35514e880481f47
Jeremy <jkaffrey@gmail.com>
2016-04-26 01:15:39 -0600

    Initial commit

---
+++ formats.js
@@ -0,0 +1,27 @@
+var iHeight = $(".small").width() + 16;
+
+$(document).ready(function() {
+
+    $(".small").hover(function() {
+
+      $(this).animate({width: '20%'});
+    }, function() {
+      $(this).animate({width: '15%'});
+    });
+
+    $(".medium").hover(function() {
+
+      $(this).animate({width: '30%'});
+    }, function() {
+
+      $(this).animate({width: '25%'});
+    });
+
+    $(".large").hover(function() {
+
+      $(this).animate({width: '40%'});
+    }, function() {
+
+      $(this).animate({width: '35%'});
+    });
+});
